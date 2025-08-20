8efa673913540847214f8e295de86c82d9febd59
Niyaz Malik (Personal) <niyaz.malik2206@gmail.com>
2025-08-20 07:00:41 +0530

    uri redirect bug resolved

--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FileController } from './file.controller';
 import { FileService } from './file.service';
+import { DownloadService } from 'src/download/download.service';
 
 @Module({
   controllers: [FileController],
-  providers: [FileService],
+  providers: [FileService, DownloadService],
 })
 export class FileModule {}
